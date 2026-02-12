93e8e74da76f9e9bad542648eb7d2d2707782b14
Victor Augusto <victor.ribeiro.silva30@escola.pr.gov.br>
2026-02-12 10:51:18 -0300

    fgdgdfhdgh

--- Teste 2.py
+++ Teste 2.py
@@ -4,8 +4,20 @@ Comentário Aleatório
 Variável. [Definição memorizada desde 2025]
 '''
 
-faturamento = 1000
+Faturamento = 1000
 
-Custo = 999
+Custo = 700
 
-print(faturamento - Custo)
+Resultado = 300
+
+print("O faturamento foi de " + Faturamento)   #Variável é um nome que se dá para algo ou coisa específica, evita ter que digitar todas as vezes que precisar editar algo.
+print("O custo foi de " + Custo)
+print("O lucro foi de " + Faturamento-Custo)   #este é um comentário. "só serve para texto" inserir, após as aspas por número.
+
+if (Resultado = Faturamento-Custo)
+
+print("Os valores inseridos estão corretos.")
+
+else
+
+print("Os valores inseridos estão incorretos.")
