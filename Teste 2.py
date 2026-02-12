8bf5434c0a29ff327913e71377a2c63f16364d57
Victor Augusto <victor.ribeiro.silva30@escola.pr.gov.br>
2026-02-12 10:59:18 -0300

    eyrthtey

--- Teste 2.py
+++ Teste 2.py
@@ -10,14 +10,14 @@ Custo = 700
 
 Resultado = 300
 
-print("O faturamento foi de " + Faturamento)   #Variável é um nome que se dá para algo ou coisa específica, evita ter que digitar todas as vezes que precisar editar algo.
-print("O custo foi de " + Custo)
-print("O lucro foi de " + Faturamento-Custo)   #este é um comentário. "só serve para texto" inserir, após as aspas por número.
+print("O faturamento foi de ", Faturamento)
+print("O custo foi de ", Custo)
+print("O lucro foi de ", Faturamento-Custo)   
 
-if (Resultado = Faturamento-Custo)
+if(Resultado == Faturamento-Custo):
 
-print("Os valores inseridos estão corretos.")
+    print("Os valores inseridos estão corretos.")
 
-else
+else:
 
-print("Os valores inseridos estão incorretos.")
+    print("Os valores inseridos estão incorretos.")
